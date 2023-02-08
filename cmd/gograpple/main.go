5254752a5a5ee10da6c1fca1a54ea536b18e32fd
Milos Pejanovic <pejan123@gmail.com>
2023-02-08 22:25:38 +0100

    move main into cmd/gograpple

---
+++ cmd/gograpple/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/foomo/gograpple/cmd/gograpple/actions"
+
+func main() {
+	actions.Execute()
+}
--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/foomo/gograpple/cmd/actions"
-
-func main() {
-	actions.Execute()
-}
